d18de4472d300887a763d3e88fb52e2c57578eee
SBlawski <48526640+Optimissedit@users.noreply.github.com>
2022-02-07 13:01:31 -0800

    DisjointSet Methods
    Added basic methods to make disjoint set structure function

--- src/DisjointSet.java
+++ src/DisjointSet.java
@@ -14,15 +14,41 @@ public class DisjointSet {
 		rank = new int [c];
 		parent = new int [c];
 		// Call function to create singleton sets for all cells
-		makeSet();
+		makeSets();
 	}
 	
-	// Function to create singleton sets for all cells in the maze
-	void makeSet() {
+	// Function to make singleton sets for all cells in the maze
+	void makeSets() {
 		for(int i = 0; i < cells; i++) {
-			// Fills the parent array with integers ascending from 0
+			// Fills the parent array with integers ascending from 0 that serve as root to trees
 			parent[i] = i;
 		}
+	}
+	
+	// Method to find the parent integer of a set
+	int findParent(int c) {
+		
+		// Given integer is parent of itself
+		if(parent[c] == c) {
+			return c;
+		}
+		else
+		{
+			// Given integer is not the parent, recursively check c's parent until found
+			return findParent(parent[c]);
+		}
+	}
+	
+	
+	// Unions two sets into a new subset
+	void unionSets(int c0, int c1) {
+		
+		// find parent of each set
+		int parent0 = findParent(c0);
+		int parent1 = findParent(c1);
+		
+		// Set parent of c0 to parent of c1 to move sets under same parent
+		parent[parent0] = parent1;
 	}
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@
 
 import java.lang.Math;
 
+
 public class Main {
 
 	public static void main(String[] args) {
